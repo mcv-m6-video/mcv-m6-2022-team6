45bc8adcac5946891eb4b2c2f9d6a8ce3af057b6
Kevin Martín <kevinmf94@gmail.com>
2022-03-13 20:14:42 +0100

    Voc?

--- Week1/main.py
+++ Week1/main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import utils as ut
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import build_detection_test_loader
 
 # PATHS
 annotations_path = 'data/ai_challenge_s03_c010-full_annotation.xml'
@@ -13,3 +15,7 @@ detections_path = {
 if __name__ == '__main__':
 	annotations = ut.read_annotations(annotations_path)
 	detections = ut.read_detections(detections_path['rcnn'])
+
+	evaluator = COCOEvaluator("balloon_val", output_dir="./output")
+	val_loader = build_detection_test_loader(cfg, "balloon_val")
+	print(inference_on_dataset(predictor.model, val_loader, evaluator))
