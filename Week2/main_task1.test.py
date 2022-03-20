47073a203731e183e8c47d0fe9ccf91558957165
Kevin Martín <kevinmf94@gmail.com>
2022-03-20 20:01:41 +0100

    Gaussian Model

--- Week2/main_task1.test.py
+++ Week2/main_task1.test.py
@@ -9,7 +9,7 @@ TRAIN_FRAMES = math.ceil(TOTAL_FRAMES * 0.25);
 TEST_FRAMES = math.ceil(TOTAL_FRAMES * 0.75);
 
 def classify_frame(u, std, frame, alpha=2.5):
-	return (frame - u >= alpha * (std + 2)).astype(np.float32)
+    return (np.abs(frame - u) >= alpha * (std + 2)).astype(np.uint8) * 255
 
 if __name__ == '__main__':
 
@@ -17,20 +17,18 @@ if __name__ == '__main__':
 	mean = np.load('../data/gaussian_model_mean.npy')
 	std = np.load('../data/gaussian_model_std.npy')
 
-	out = cv2.VideoWriter('../data/outpy.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (1920, 1080))
 	for i in range(TRAIN_FRAMES, TEST_FRAMES):
-		frame = cv2.imread(frames_folder + "%04d.jpeg" % i, cv2.IMREAD_GRAYSCALE);
-		classify = classify_frame(mean, std, frame, alpha=0.2)
+		frame = cv2.imread(frames_folder + "%04d.jpeg" % i, cv2.IMREAD_GRAYSCALE)
+		classify = classify_frame(mean, std, frame, alpha=3)
+
+		kernel = np.ones((5, 5), np.uint8)
+		opening = cv2.morphologyEx(classify, cv2.MORPH_OPEN, kernel)
 
 		# frameRes = cv2.resize(frame, (960, 540))
 		# cv2.imshow('Frame', frameRes)
 
-		classiRes = cv2.resize(classify, (960, 540))
-		out.write(classify.astype(np.uint8))
+		classiRes = cv2.resize(opening, (960, 540))
 		cv2.imshow('Class', classiRes)
 		if cv2.waitKey(25) & 0xFF == ord('q'):
 			cv2.destroyAllWindows()
 			break
-
-	cv2.destroyAllWindows()
-	out.release()
