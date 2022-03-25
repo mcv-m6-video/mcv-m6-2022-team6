11cf2d36a2688d99e3b8d9fcca96f59a77c8eecb
servidal <91950717+servidal@users.noreply.github.com>
2022-03-25 12:02:31 +0100

    Add files via upload

--- Week2/bg_est.py
+++ Week2/bg_est.py
@@ -7,94 +7,121 @@ import voc_evaluation
 import utils_week1, utils_week2
 from utils_week1 import read_annotations
 from utils_week2 import nms
+import imageio
+
 
 annotations_path = 'data/ai_challenge_s03_c010-full_annotation.xml'
 roi_path = 'data/AICity_data/train/S03/c010/roi.jpg'
-result_path = 'week2/output/'
+result_path = 'Week2/output/'
 
 
-def adaptive_bg_est(image, frame_size, mean, std, alpha, rho):
-    alpha = alpha
-    rho = rho
+def adaptive_bg_est(image, frame_size, mean, std, alpha, rho): #adaptive modelling
+
     h, w = frame_size
-    mask = abs(image - mean) >= alpha * (std + 2)
+    mask = abs(image - mean) >= alpha * (std + 2)  #foreground pixels (Bool: true or false)
 
-    segmentation = np.zeros((h, w))
-    segmentation[mask] = 255
+    segmentation = np.zeros((h, w)) #black "image"
+    segmentation[mask] = 255  #gives white color to those foreground pixels
 
-    mean = np.where(mask, mean, rho * image + (1 - rho) * mean)
+    #Adaptive modelling
+    mean = np.where(mask, mean, rho * image + (1 - rho) * mean)  #np.where(condition[,x,y]) -> when True, yield x(foreground), otherwise y(background)
     std = np.where(mask, std, np.sqrt(rho * (image - mean) ** 2 + (1 - rho) * std ** 2))
 
     return segmentation, mean, std, alpha
 
-def morph_filter(img):
+def morph_filter(seg):
 
-    img = cv2.GaussianBlur(img, (5, 5), 10)
-    img = cv2.dilate(img, np.ones((3, 3)))
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    img = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel, iterations=6)
+    kernel = np.ones((2, 2), np.uint8)
+    seg = cv2.erode(seg, kernel, iterations=1)
+    kernel = np.ones((3,4), np.uint8)
+    seg = cv2.dilate(seg, kernel, iterations=1)
 
-    return img
+    seg = cv2.morphologyEx(seg, cv2.MORPH_OPEN, np.ones((7, 4), np.uint8))
+    seg = cv2.morphologyEx(seg, cv2.MORPH_CLOSE, np.ones((4, 7), np.uint8))
 
+    return seg
+
+
+def fg_bboxes(seg, frame_id):
+    bboxes = {}
+    roi = cv2.imread(roi_path, cv2.IMREAD_GRAYSCALE) / 255
+    segmentation = seg * roi  #convert to a binary image to use findContours()
+    contours, _ = cv2.findContours(segmentation.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #cv2.drawContours(segmentation, contours,-1,(0,0,255),3)
+    #cv2.imshow("img", segmentation)
+    
+    frameId = str(frame_id)
+    idx = 0
+    for c in contours:
+        rect = cv2.boundingRect(c)  
+        if rect[2] < 50 or rect[3] < 50 or rect[2]/rect[3] < 0.8:  # rect[2]= width, rect[3]=height
+            continue  # Discard small contours
+
+        x, y, w, h = rect #return: top left corner coord (xlt,ylt), width and height of the rectangle
+        bboxes[idx] = []
+        bboxes[idx].append({
+                    "bbox": np.array([float(x), float(y), float(x) + float(w), float(y) + float(h)]),
+                    "conf": None,
+                    "difficult": False,
+                    "frame": frame_id
+                })
+
+        idx += 1
+
+    return bboxes
 
 def eval(vidcap, frame_size, mean, std, test_len, alpha, rho):
     annotations_read = read_annotations(annotations_path)
-    init_frame = int(vidcap.get(cv2.CAP_PROP_POS_FRAMES))
-    frame_id = init_frame
+    init_frame = int(vidcap.get(cv2.CAP_PROP_POS_FRAMES))  #get frame position (535)
+    frame_id = init_frame  #frame number
+    
     detections = {}
     annotations = {}
-    
+    idx=0
+    #writer = imageio.get_writer("Week2/bboxes.gif", mode="I") #gift
     for t in tqdm(range(test_len)):
-        _, frame = vidcap.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #cv2.imshow('Frame', frame)    
-        segmentation, mean, std, alpha = adaptive_bg_est(frame, frame_size, mean, std, alpha, rho)
-        #cv2.imshow('Segmentation', segmentation)
-        roi = cv2.imread(roi_path, cv2.IMREAD_GRAYSCALE) / 255
-        segmentation = segmentation * roi
-        #cv2.imshow('Segmentation*roi', segmentation)
-        segmentation = morph_filter(segmentation).astype(np.uint8)
-        #cv2.imshow('Filtered seg', segmentation)
-
-        #save
-        #if frame_id >= 1169 and frame_id < 1229 : # if frame_id >= 535 and frame_id < 550
-        #    cv2.imwrite(result_path + f"seg_{str(frame_id)}_pp_{str(alpha)}.bmp", segmentation.astype(int))
-
-        output = cv2.connectedComponentsWithStats(segmentation)
         
-        frameRes = frame
+        _, frame = vidcap.read() #read a frame
+        #cv2.imshow('Colored Frame', frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #cv2.imshow('Grayscaled Frame', frame) # Display the resulting frame
+          
+        segmentation, mean, std, alpha = adaptive_bg_est(frame, frame_size, mean, std, alpha, rho) #apply adaptive modelling
+        #cv2.imshow('Segmentation', segmentation)
+        
+        roi = cv2.imread(roi_path, cv2.IMREAD_GRAYSCALE) / 255  #region of interest (0's and 1's)
+        segmentation = segmentation * roi  #select foreground pixels of region of interest (remove noise)
+        #cv2.imshow('Segmentation of region of interest', segmentation)
+        
+        segmentation = morph_filter(segmentation).astype(np.uint8) #apply morphological filter
+        #cv2.imshow('Filtered segmentation', segmentation)
 
-        bboxes = []
-        for i_region in range(len(output[2])):
-            if output[2][i_region][4] > 5000 and output[2][i_region][3] < 2000:
-                x1 = output[2][i_region][0]
-                y1 = output[2][i_region][1]
-                x2 = x1 + output[2][i_region][2]
-                y2 = y1 + output[2][i_region][3]
-                bboxes.append([x1, y1, x2, y2, output[2][i_region][4]])
+        #Detect foreground bounding boxes of the frame
+        det_bboxes = fg_bboxes(segmentation, frame_id)
+        
+        #Detections dict
+        for det in det_bboxes:
+            detections[idx] = det_bboxes[det]
+            idx = idx+1
+        
+        #annotations dict
+        gt_bboxes = []
+        if frame_id in annotations_read:
+            gt_bboxes = annotations_read[frame_id]
+        annotations[frame_id] = gt_bboxes
 
-        frameId = str(frame_id)
-
-        annotations[frameId] = annotations_read[frame_id]
-        if len(bboxes) > 0:
-            bboxes = np.array(bboxes)
-            keep = nms(bboxes, 0.1)
-            detections[frameId] = []
-            for bbox in bboxes[keep]:
-                frameRes = cv2.rectangle(frameRes, (bbox[0], bbox[1]) , (bbox[2], bbox[3]), (0,0,255))
-                detections[frameId].append({
-                    "bbox": np.array(bbox),
-                    "conf": 1.0,
-                    "frame": str(frame_id)
-                })
+        #annotations[frameId] = annotations_read[frame_id]
+    
 
         #Show boxes
+        #frameRes = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
         #frameRes = cv2.resize(frameRes, (960, 540))
-        #cv2.imshow('Frame with boxes', frame)
+        #cv2.imshow('Frame with boxes', frameRes)
         #if cv2.waitKey(10) & 0xFF == ord('q'):
         #    cv2.destroyAllWindows()
         #    break
 
+        #writer.append_data(seg_boxes.astype(np.uint8))
         frame_id += 1
 
     rec, prec, ap = voc_evaluation.voc_eval(annotations, detections, use_confidence=False)
@@ -105,28 +132,14 @@ def eval(vidcap, frame_size, mean, std, test_len, alpha, rho):
 def train(vidcap, frame_size, train_len):
     count = 0
     h, w = frame_size
-    #nc = color_space[params['color_space']][1]
-    #if nc == 1:
+    
     mean = np.zeros((h, w))
     M2 = np.zeros((h, w))
-    #else:
-    #    mean = np.zeros((h, w, nc))
-    #    M2 = np.zeros((h, w, nc))
 
-    # Compute mean and std
+    # Compute mean and std for training frames
     for t in tqdm(range(train_len)):
         _, frame = vidcap.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #frame = cv2.cvtColor(frame, color_space[params['color_space']][0])
-        #if params['color_space'] == 'H':
-        #    H,S,V = np.split(frame,3,axis=2)
-        #    frame=np.squeeze(H)
-        #if params['color_space'] == 'L':
-        #    L,A,B = np.split(frame,3,axis=2)
-        #    frame=np.squeeze(L)
-        #if params['color_space'] == 'CbCr':
-        #    Y,Cb,Cr= np.split(frame,3,axis=2)
-        #    frame=np.dstack((Cb,Cr))
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #convert frame to grayscale
         count += 1
         delta = frame - mean
         mean += delta / count
--- Week2/main_task2.py
+++ Week2/main_task2.py
@@ -7,11 +7,11 @@ from bg_est import train, eval
 import matplotlib.pyplot as plt
 video_path = 'data/AICity_data/train/S03/c010/vdo.avi'
 
-vidcap = cv2.VideoCapture(video_path)
-frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
-frame_width = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-frame_size = [frame_height,frame_width]
+vidcap = cv2.VideoCapture(video_path)  #Reading input file
+frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))    #number of total frames
+frame_width = int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH))    #width frame
+frame_height = int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT))  #height frame
+frame_size = [frame_height,frame_width]                    #frame size 
 print("Total frames: ", frame_count)
 
 train_len = int(0.25 * frame_count) #First 25% frames for training
@@ -24,31 +24,30 @@ mean, std = train(vidcap, frame_size, train_len)
 
 # Evaluate
 #Grid-search
-alpha = np.arange(3, 8, 1).tolist() 
-rho = np.arange(0.01, 0.06, 0.01).tolist()  
+alpha = 3 #np.arange(3, 8, 1).tolist() 
+rho = 0.02 #np.arange(0.001, 0.01, 0.004).tolist()  
 
-all_combinations = list(itertools.product(alpha, rho))
-print(all_combinations)
-print("Num. of comb.", len(all_combinations))
+#all_combinations = list(itertools.product(alpha, rho))
+#print(all_combinations)
+#print("Num. of comb.", len(all_combinations))
 
 ap = []
-i=0
-for alpha, rho in all_combinations:
-    if i>0:
-        vidcap = cv2.VideoCapture(video_path)
-    ap.append(eval(vidcap, frame_size, mean, std, test_len, alpha, rho))
-    print(i)
-    print(ap)
-    i+=1
+#i=0
+#for alpha, rho in all_combinations:
+    #if i>0:
+    #    vidcap = cv2.VideoCapture(video_path)
+ap.append(eval(vidcap, frame_size, mean, std, test_len, alpha, rho))
+    #print(i)
 print(ap)
 
-max_position = ap.index(max(ap))
-print('The maximum accuracy is obtained with a value of {} with alpha {} and rho {}'.format(np.round(ap[max_position],5),all_combinations[max_position][0],all_combinations[max_position][1]))
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.scatter([combi[0] for combi in all_combinations], [combi[1] for combi in all_combinations], ap)
-ax.set_xlabel('alpha')
-ax.set_ylabel('rho')
-ax.set_zlabel('mAP')
-plt.show()
+#max_position = ap.index(max(ap))
+#print('The maximum accuracy is obtained with a value of {} with alpha {} and rho {}'.format(np.round(ap[max_position],5),all_combinations[max_position][0],all_combinations[max_position][1]))
+
+#fig = plt.figure()
+#ax = fig.add_subplot(projection='3d')
+#ax.scatter([combi[0] for combi in all_combinations], [combi[1] for combi in all_combinations], ap)
+#ax.set_xlabel('alpha')
+#ax.set_ylabel('rho')
+#ax.set_zlabel('mAP')
+#plt.show()
