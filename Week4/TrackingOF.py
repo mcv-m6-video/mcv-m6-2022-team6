a705b1e9e86eedb1749adb213e5b310d42b2df6f
Kevin Martín <kevinmf94@gmail.com>
2022-04-05 20:52:59 +0200

    Tracking OF

---
+++ Week4/TrackingOF.py
@@ -0,0 +1,149 @@
+import copy
+
+import numpy as np
+
+from Week3 import get_rect_iou
+from Week3.Tracking import TrackingBase
+import cv2
+
+# params for ShiTomasi corner detection
+feature_params = dict(maxCorners=100,
+					  qualityLevel=0.3,
+					  minDistance=7,
+					  blockSize=7)
+# Parameters for lucas kanade optical flow
+lk_params = dict(winSize=(15, 15),
+				 maxLevel=2,
+				 criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+
+class TrackingOF(TrackingBase):
+	first_frame = True
+	prev_frame = None
+	mask = None
+	p0 = None
+	good_new = []
+	good_old = []
+	near_dis = 100
+
+	def __init__(self, gt_ids, gt_bbox, iou_th=0.5):
+		super().__init__(gt_ids, gt_bbox, iou_th)
+
+	def opticalLukasKanadeInit(self, img_gray):
+		self.prev_frame = img_gray
+		self.p0 = cv2.goodFeaturesToTrack(self.prev_frame, mask=None, **feature_params)
+		self.mask = np.zeros_like(img_gray)
+		self.first_frame = False
+
+	def opticalLukasKanade(self, img_gray):
+
+		# calculate optical flow
+		p1, st, err = cv2.calcOpticalFlowPyrLK(self.prev_frame, img_gray, self.p0, None, **lk_params)
+
+		# Select good points
+		if p1 is not None:
+			self.good_new = p1[st == 1]
+			self.good_old = self.p0[st == 1]
+
+		# draw the tracks
+		"""for i, (new, old) in enumerate(zip(self.good_new, self.good_old)):
+			a, b = new.ravel()
+			c, d = old.ravel()
+			self.mask = cv2.line(self.mask, (int(a), int(b)), (int(c), int(d)), self._colours[i].tolist(), 2)"""
+
+		self.p0 = self.good_new.reshape(-1, 1, 2)
+		flow = (self.good_new - self.good_old);
+
+		return flow, self.p0, self.mask
+
+	def drawLukasKanade(self, img):
+
+		for i, (new, old) in enumerate(zip(self.good_new, self.good_old)):
+			a, b = new.ravel()
+			img = cv2.circle(img, (int(a), int(b)), 5, self._colours[i].tolist(), -1)
+
+		return img
+
+	def iou_tracking(self, frame, bboxes):
+
+		new_frame = []
+		id_selected = []
+		for bbox in bboxes:
+			bbox_norm = bbox[0:4].astype(np.int32)
+			track_selected = self.find_best_iou(bbox_norm, self._last_frame, id_selected)
+
+			if track_selected:
+				id_selected.append(track_selected['id'])
+				track_selected['bbox'] = bbox_norm;
+				track_selected['frame'] = frame;
+				self._tracks[track_selected['id']].append(track_selected)
+				new_frame.append(track_selected);
+			else:
+				assignedId = self.generate_id();
+				self._tracks[assignedId] = [];
+				newTrack = {"id": assignedId, "frame": frame, "bbox": bbox_norm,
+							"color": self._colours[(assignedId - 1) % 2000],
+							'center': (bbox_norm[0:2] + bbox_norm[2:4]) // 2}
+				self._tracks[assignedId].append(newTrack)
+				new_frame.append(newTrack);
+
+		self.update_metrics(frame, self._last_frame)
+		self._last_frame = new_frame;
+
+		return new_frame
+
+	def generate_track(self, frame, bboxes, img_gray):
+
+		new_frame = []
+		id_selected = []
+
+		if self.first_frame:
+			self.opticalLukasKanadeInit(img_gray)
+			return self.iou_tracking(frame, bboxes), self.mask
+		else:
+			flow, p0, mask = self.opticalLukasKanade(img_gray)
+
+			for bbox in bboxes:
+				bbox_norm = bbox.astype(np.int32)[0:4];
+				best_track = None
+				best_tack_iou = 9999
+
+				for track in self._last_frame:
+
+					if track['id'] in id_selected:
+						continue
+
+					center = track['center']
+					near_points = np.sqrt(np.sum(np.square(center - p0), axis=2)) < self.near_dis
+
+					if np.sum(near_points) > 0:
+						movement = flow[near_points.flatten(), :].mean(axis=0)
+
+						# Generate temp bbox (move in flow direction)
+						bbox_of = track['bbox'].copy()
+						bbox_of[0:2] = bbox_of[0:2] + movement
+						bbox_of[2:4] = bbox_of[2:4] + movement
+
+						# Check if best
+						iou = get_rect_iou(bbox_norm, bbox_of);
+						if iou > 0.5 and iou > best_tack_iou:
+							best_track = track
+							best_tack_iou = iou
+
+				if best_track is None:
+					assignedId = self.generate_id();
+					self._tracks[assignedId] = [];
+					newTrack = {"id": assignedId, "frame": frame, "bbox": bbox_norm,
+								"color": self._colours[(assignedId - 1) % 2000],
+								'center': (bbox_norm[0:2] + bbox_norm[2:4]) // 2}
+					self._tracks[assignedId].append(newTrack)
+					new_frame.append(newTrack)
+				else:
+					track_cpy = copy.deepcopy(best_track)
+					id_selected.append(track_cpy['id'])
+					track_cpy['bbox'] = bbox_norm
+					track_cpy['frame'] = frame
+					self._tracks[track['id']].append(track_cpy)
+					new_frame.append(track_cpy)
+
+		return new_frame, self.mask
--- Week4/task1_3.py
+++ Week4/task1_3.py
@@ -2,9 +2,10 @@ import argparse
 import numpy as np
 
 from Week3 import utils_week3 as uw3
-from Week3.Tracking import TrackingIOU, TrackingKalman, TrackingKalmanSort, TrackingIOUDirection
 import imageio
 import cv2
+
+from TrackingOF import TrackingOF
 
 annotations_path = '../data/ai_challenge_s03_c010-full_annotation.xml'
 TOTAL_FRAMES = 2141
@@ -16,19 +17,9 @@ def draw_bbox(img, bbox, id=1, color=(0, 0, 255)):
 	return cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 3)
 
 
-def to_flow_view(img, flow):
-	mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-	hsv = np.zeros_like(img)
-	hsv[..., 1] = 255
-	hsv[..., 0] = ang * 180 / np.pi / 2
-	hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-	return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Car tracking")
 	parser.add_argument('--iou_th', default=0.3)
-	parser.add_argument('--tracker', default='kalmansort')
 	parser.add_argument('--input', default='faster101')
 	parser.add_argument('--preview', default=True)
 	args = parser.parse_args()
@@ -38,14 +29,7 @@ if __name__ == '__main__':
 	else:
 		gt_ids, annotations = uw3.read_annotations(annotations_path, True);
 
-	if args.tracker == "iou":
-		tracker = TrackingIOU(gt_ids, annotations, args.iou_th);
-	elif args.tracker == "iou_direction":
-		tracker = TrackingIOUDirection(gt_ids, annotations, args.iou_th);
-	elif args.tracker == "kalman":
-		tracker = TrackingKalman(gt_ids, annotations, args.iou_th);
-	elif args.tracker == "kalmansort":
-		tracker = TrackingKalmanSort(gt_ids, annotations, args.iou_th)
+	tracker = TrackingOF(gt_ids, annotations, args.iou_th)
 
 	if args.input == "gt":
 		detections = annotations
@@ -59,8 +43,7 @@ if __name__ == '__main__':
 		detections = uw3.read_detections('../data/detections/faster_rcnn_X_101_32x8d_FPN_3x/detections.txt');
 
 	# writer = imageio.get_writer("results/task22_overlap_cars.gif", mode="I")
-	prev_frame = cv2.imread("../data/images/%04d.jpeg" % 1, cv2.IMREAD_GRAYSCALE)
-	for i in range(1, TOTAL_FRAMES):
+	for i in range(0, TOTAL_FRAMES):
 		frameId = i + 1;
 		if i % 100 == 0:
 			print("Frame %04d of %04d" % (frameId, TOTAL_FRAMES))
@@ -69,10 +52,8 @@ if __name__ == '__main__':
 		img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY);
 		bboxes = detections[i]
 
-		flow = cv2.calcOpticalFlowFarneback(prev_frame, img_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+		tracks, mask = tracker.generate_track(i, bboxes, img_gray)
 
-		tracks = tracker.generate_track(i, bboxes)
-		prev_frame = img_gray.copy()
 		if args.preview:
 			if len(tracks) > 0:
 				for track in tracks:
@@ -80,11 +61,12 @@ if __name__ == '__main__':
 
 			# Show
 			# writer.append_data(cv2.cvtColor(cv2.resize(img, (1920//2, 1080//2)), cv2.COLOR_BGR2RGB))
-			cv2.imshow('Video', cv2.resize(to_flow_view(img, flow), (1920 // 2, 1080 // 2)))
+			cv2.imshow('Video', cv2.resize(img, (1920 // 2, 1080 // 2)))
+			# cv2.imshow('Flow', cv2.resize(to_flow_view(img, flow), (1920 // 2, 1080 // 2)))
 			if cv2.waitKey(10) & 0xFF == ord('q'):
 				cv2.destroyAllWindows()
 				break
 
 	# writer.close()
 	results = tracker.get_IDF1()
-	print(results);
+	print(results)
