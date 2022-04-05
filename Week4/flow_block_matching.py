205b10c172947d1cd2fb74e4c0b562ad1ed60daa
eudaldbc <49155392+eudaldbc@users.noreply.github.com>
2022-04-05 12:33:07 +0200

    Update flow_block_matching.py

--- Week4/flow_block_matching.py
+++ Week4/flow_block_matching.py
@@ -11,8 +11,11 @@ def exhaustive_search_cv2(template: np.ndarray, target: np.ndarray, metric='cv2.
 	"""
 	# evaluate the openCV metric
 	metric = eval(metric)
-	result = cv2.matchTemplate(template, target, metric)
+	result = cv2.matchTemplate(target, template, metric)
 	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+	cv2.imshow("target",target)
+	cv2.imshow("sqare",template)
+	cv2.imshow("result",result)
 
 	if metric in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
 		pos = min_loc
@@ -67,7 +70,7 @@ def logarithmic_search(template: np.ndarray, target: np.ndarray, step=4):
 
 
 def get_optical_flow(img1: np.ndarray, img2: np.ndarray, block_size=32, area=32, mode="forward",
-					 method="log", log_step=4, metric="SSD", show_preview=False):
+					 method="log", show_preview=False, log_step = 4):
 
 	if mode == "backward":
 		img1, img2 = img2, img1
@@ -77,35 +80,39 @@ def get_optical_flow(img1: np.ndarray, img2: np.ndarray, block_size=32, area=32,
 
 	for ih in range(0, h - block_size, block_size):
 		for iw in range(0, w - block_size, block_size):
+			top_l = (iw, ih);
+			top_l_search = (max(iw - area, 0), max(ih - area, 0));
+			bot_r = (iw + block_size, ih + block_size);
+			bot_r_search = (min(bot_r[0] + area, w), min(bot_r[1] + area, h));
 
-			top_left_s = (max(ih - area, 0), max(iw - area, 0))
-			bottom_right_s = min(ih + block_size + area, h), min(iw + block_size + area, w)
-
-			patch = img1[ih:ih + block_size, iw:iw + block_size];
-			search_patch = img2[top_left_s[0]:bottom_right_s[0], top_left_s[1]:bottom_right_s[1]]
+			patch = img1[top_l[1]:bot_r[1], top_l[0]:bot_r[0]];
+			search_patch = img2[top_l_search[1]:bot_r_search[1], top_l_search[0]:bot_r_search[0]]
 
 			if method == "cv2":
-				displacement = exhaustive_search_cv2(patch, search_patch, metric)
+				displacement  = exhaustive_search_cv2(patch, search_patch)
 			elif method == "log":
 				displacement = logarithmic_search(patch, search_patch, step=log_step)
 			elif method == "full":
-				displacement = exhaustive_search_own(patch, search_patch, block_size, metric)
+				displacement = exhaustive_search_own(patch, search_patch, block_size)
 
-			v_flow = int(displacement[1]) - (iw - top_left_s[1])
-			u_flow = int(displacement[0]) - (ih - top_left_s[0])
+			v_flow = int(displacement[1]) - (top_l[1]- top_l_search[1])
+			u_flow = int(displacement[0]) - (top_l[0]- top_l_search[0])
 			flow[ih:ih + block_size, iw:iw + block_size] = [u_flow, v_flow]
 
 			if show_preview:
 				preview = img1.copy()
-				top_l = (iw, ih)
-				bot_r = (iw + block_size, ih + block_size)
-				# preview = cv2.rectangle(preview, top_left_s, bottom_right_s, (255, 0, 0));
+				preview = cv2.rectangle(preview, top_l_search, bot_r_search, (255, 0, 0));
 				preview = cv2.rectangle(preview, top_l, bot_r, (0, 0, 255));
 				preview = cv2.rectangle(preview, (u_flow + top_l[0], v_flow + top_l[1]),
 										(u_flow + top_l[0] + block_size, v_flow + top_l[1] + block_size), (0, 255, 0));
-				# preview = cv2.drawMarker(preview, (u_flow + top_l[0], v_flow + top_l[1]), (0, 255, 0))
+				#preview = cv2.drawMarker(preview, (u_flow + top_l[0], v_flow + top_l[1]), (0, 255, 0))
 				cv2.imshow("Preview", preview)
-				cv2.waitKey(0);
+				preview2 = img2.copy()
+				preview2 = cv2.rectangle(preview2, (u_flow + top_l[0], v_flow + top_l[1]),
+										(u_flow + top_l[0] + block_size, v_flow + top_l[1] + block_size), (0, 255, 0));
+
+				cv2.imshow("Preview2",preview2)
+				#cv2.waitKey(0) 
 
 	flow = np.dstack((flow[:, :, 0], flow[:, :, 1], np.ones_like(flow[:, :, 0])))
 	return flow
