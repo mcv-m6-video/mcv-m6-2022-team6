f2163bf44d0443b8d118cabdff5abfc0ef45e690
servidal <91950717+servidal@users.noreply.github.com>
2022-04-12 16:57:23 +0200

    Add files via upload

--- Week5/patches_crop.py
+++ Week5/patches_crop.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 import sys
 import csv
 
+
 sys.path.append("Week3")
 from utils_week3 import read_detections2
 
@@ -15,49 +16,50 @@ def mylistdir(directory):
     return [x for x in filelist
             if not (x.startswith('.'))]
 
-def create_csv_patches(video_path, det_path, patches_path, sequence, camera, writer):
+def create_csv_patches(video_path, gt_path, patches_path, sequence, camera, writer):
     vidcap = cv2.VideoCapture(video_path)
     num_frames = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
     print("Total frames: ", num_frames)
 
-    det = read_detections2(det_path)
+    gt = read_detections2(gt_path)
 
     for frame_id in tqdm(range(num_frames), desc='Creating patches of seq ' + sequence + '/' + camera):
         _, frame = vidcap.read()
-        if frame_id in det:
-            det_bboxes = det[frame_id]
+        if frame_id in gt:
+            gt_bboxes = gt[frame_id]
 
-            for box in det_bboxes:
+            for box in gt_bboxes:
+                
                 crop_img = frame[int(box['bbox'][1]):int(box['bbox'][3]), int(box['bbox'][0]):int(box['bbox'][2])]
-                cv2.imwrite(patches_path + f"/{str(box['id'])}_{sequence}_{camera}_{str(frame_id)}.jpg",
+                cv2.imwrite(patches_path + f"/{str(box['id'])}_{sequence}_{camera}_{str(frame_id)}.jpg",            #Save gt patches
                              crop_img.astype(int))
-                center=[(box['bbox'][0] + box['bbox'][2]) // 2, (box['bbox'][1] + box['bbox'][3]) // 2]
+                
                 filename = str(box['id']) + '_' + sequence + '_' + camera + '_' + str(frame_id) + '.jpg'
-                writer.writerow([filename, str(box['id']), sequence, camera, str(frame_id), str(box['bbox'][0]), str(box['bbox'][1]),
-                                 str(box['bbox'][2]), str(box['bbox'][3]), str(center[0]), str(center[1])])
+                writer.writerow([filename, str(box['id'])])
 
         frame_id += 1
 
     return
 
 if __name__ == "__main__":
-    sequence = 'S03'
-    videos_path = os.path.join('data/AICity_data/train', sequence)
-    detections_path = os.path.join('data/detections/retina', sequence)
-    patches_path =  os.path.join('patches', sequence)
-    os.makedirs(patches_path, exist_ok=True)
-
-    with open('car_patches_annotations.csv', 'w', newline='') as file:
+    
+    sequences = ['S03','S04','S01']
+    with open('Week5/gt/gt_car_patches_annotations.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(
-            ["FILENAME", "ID", "SEQUENCE", "CAMERA", "FRAME", "XTL", "YTL", "XBR", "YBR", "CENTER_X", "CENTER_Y"])  # header
+            ["FILENAME", "ID"])  
+        
+        for seq in sequences:
+            videos_path = os.path.join('data/AICity_data/train', seq)
+            patches_path =  os.path.join('Week5/gt/gt_car_patches', seq)
+            os.makedirs(patches_path, exist_ok=True)
+            
+            for camera in sorted(mylistdir(videos_path)):
+                gt_path = os.path.join(videos_path, camera, 'gt/gt.txt')
+                video_path = os.path.join(videos_path, camera, 'vdo.avi')
 
-        for camera in sorted(mylistdir(detections_path)):
-            det_path = os.path.join(detections_path, camera, 'overlap_filtered_detections.txt')
-            video_path = os.path.join(videos_path, camera, 'vdo.avi')
+                print("--------------- VIDEO ---------------")
+                print(seq, camera)
+                print("--------------------------------------")
 
-            print("--------------- VIDEO ---------------")
-            print(sequence, camera)
-            print("--------------------------------------")
-
-            create_csv_patches(video_path, det_path, patches_path, sequence, camera, writer)
+                create_csv_patches(video_path, gt_path, patches_path, seq, camera, writer)
