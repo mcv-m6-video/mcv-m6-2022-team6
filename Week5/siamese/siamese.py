53d3828f939fa2dfc97d47fab9cddf0f104b5d8f
Kevin Martín <kevinmf94@gmail.com>
2022-04-13 19:23:48 +0200

    Fix not train

--- Week5/siamese/siamese.py
+++ Week5/siamese/siamese.py
@@ -7,27 +7,43 @@ class Siamese(nn.Module):
 
 	def __init__(self):
 		super(Siamese, self).__init__()
-		""",self.conv = nn.Sequential(
-			nn.Conv2d(1, 64, 10),  # 64@96*96
+		self.cnn1 = nn.Sequential(
+			nn.Conv2d(3, 96, kernel_size=11, stride=1),
 			nn.ReLU(inplace=True),
-			nn.MaxPool2d(2),  # 64@48*48
-			nn.Conv2d(64, 128, 7),
-			nn.ReLU(),    # 128@42*42
-			nn.MaxPool2d(2),   # 128@21*21
-			nn.Conv2d(128, 128, 4),
-			nn.ReLU(), # 128@18*18
-			nn.MaxPool2d(2), # 128@9*9
-			nn.Conv2d(128, 256, 4),
-			nn.ReLU(),   # 256@6*6
-		)"""
-		self.conv = models.resnet18(pretrained=True);
-		#self.liner = nn.Sequential(nn.Linear(9216, 4096), nn.Sigmoid())
-		self.out = nn.Linear(1000, 1)
+			nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+			nn.MaxPool2d(3, stride=2),
+
+			nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+			nn.ReLU(inplace=True),
+			nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+			nn.MaxPool2d(3, stride=2),
+			nn.Dropout2d(p=0.3),
+
+			nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+			nn.ReLU(inplace=True),
+
+			nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+			nn.ReLU(inplace=True),
+			nn.MaxPool2d(3, stride=2),
+			nn.Dropout2d(p=0.3),
+		)
+
+		# Defining the fully connected layers
+		self.fc1 = nn.Sequential(
+			nn.Linear(6400, 1024),
+			nn.ReLU(inplace=True),
+			nn.Dropout2d(p=0.5),
+
+			nn.Linear(1024, 128),
+			nn.ReLU(inplace=True),
+
+			nn.Linear(128, 2)
+		)
 
 	def forward_one(self, x):
-		x = self.conv(x)
+		x = self.cnn1(x)
 		x = x.view(x.size()[0], -1)
-		#x = self.liner(x)
+		x = self.fc1(x)
 		return x
 
 	def forward(self, x1, x2):
